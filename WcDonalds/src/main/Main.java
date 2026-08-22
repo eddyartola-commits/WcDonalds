fc00c8e4181f20a2676777753ab09ae1b9a1d823
eddy <eddyartola@emilianisomascos.edu.gt>
2026-08-21 21:13:30 -0600

    Cambios para baten

---
+++ WcDonalds/src/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+import GUI.Login;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Login interfaz = new Login();
+        interfaz.setVisible(true);
+        
+        
+    }
+    
+}
